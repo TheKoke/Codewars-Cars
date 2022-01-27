997b6c32bdeac855b379a62d8b761b2081031127
Damir Issayev <82366417+TheKoke@users.noreply.github.com>
2022-01-27 18:17:22 +0600

    Cars #2

---
+++ Cars #2/interfaces/IDrivingDisplay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cars
+{
+    public interface IDrivingDisplay
+    {
+        int ActualSpeed { get; }
+    }
+}
---
+++ Cars #2/interfaces/IEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cars
+{
+    public interface IEngine
+    {
+        bool IsRunning { get; }
+
+        void Consume(double liters);
+
+        void Start();
+
+        void Stop();
+    }
+}
